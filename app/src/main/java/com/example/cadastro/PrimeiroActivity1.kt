ab5074ab57165b4c996dc9ed54579796487592d5
Weslley <weslleylins2015@gmail.com>
2022-07-01 19:34:36 -0300

    Initial commit

---
+++ app/src/main/java/com/example/cadastro/PrimeiroActivity1.kt
@@ -0,0 +1,13 @@
+package com.example.cadastro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PrimeiroActivity1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_primeiro1)
+    }
+
+
+}
---
+++ app/src/main/java/com/example/cadastro/Segundactivity.kt
@@ -0,0 +1,11 @@
+package com.example.cadastro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Segundactivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_segundactivity)
+    }
+}
